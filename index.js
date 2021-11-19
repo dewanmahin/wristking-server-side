3f6206c352f069d0e4a0cb74f833db14064d13cc
DEWAN <dewanmahinchy@gmail.com>
2021-11-19 21:51:09 +0600

    final commit all complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -18,6 +19,15 @@ async function run () {
         const database = client.db('wristKing');
         const productsCollection = database.collection("products");
         const ordersCollection = database.collection("orders");
+        const reviewsCollection = database.collection("reviews");
+        const usersCollection = database.collection("users");
+
+        // // POST PACKAGE API
+        app.post('/addProduct', async (req, res) => {
+            addProduct = req.body;
+            const result = await productsCollection.insertOne(addProduct);
+            res.json(result);
+        })
 
         // // GET PRODUCTS API
         app.get('/products', async (req, res) => {
@@ -33,12 +43,81 @@ async function run () {
             res.json(result)
         })
 
-        // // GET All Order API 
+        // // GET ALL ORDER API
         app.get('/allOrders', async (req, res) => {
             const cursor = ordersCollection.find({});
             const allOrders = await cursor.toArray();
             res.send(allOrders);
         })
+
+        // // GET MY ORDER API
+        app.get('/allOrders/:userEmail', async(req, res) => {
+            const email = req.params.userEmail;
+            const query = {userEmail:email}
+            const cursor = ordersCollection.find(query)
+            const result = await cursor.toArray();
+            res.json(result);
+        })
+
+        // // DELETE ORDER API 
+        app.delete('/allOrders/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        // // DELETE ORDER API 
+        app.delete('/products/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productsCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        // // POST REVIEW API
+        app.post('/reviews', async (req, res) => {
+            review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.json(result);
+        })
+
+        // // GET REVIEW API
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const allReviews = await cursor.toArray();
+            res.send(allReviews);
+        })
+
+        // // POST USER API
+        app.post('/users', async(req, res) => {
+            user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        })
+
+        // PUT USER ADMIN API
+        app.put('/users/admin', async(req, res) => {
+            const user = req.body;
+            const filter = {email: user.email};
+            const updateDoc = {$set: {role: 'admin'}};
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+
+        // // GET USER API
+        app.get('/users/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if(user?.role == 'admin'){
+                isAdmin = true;
+            }
+            res.json({admin: isAdmin});
+        })
+
 
     }finally{
         // await client.close();
@@ -48,7 +127,7 @@ async function run () {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send("server is running")
+    res.send("wristKing server is running")
 })
 
 app.listen(port, () => {
